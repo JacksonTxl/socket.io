1605cdab6661516cdfe4c6c4e1d291360878d2e9
JacksonTxl <172004133@qq.com>
2019-05-08 14:02:12 +0800

    update dinner

--- server.js
+++ server.js
@@ -32,28 +32,30 @@ io.on('connection', function (socket) {
       io.sockets.emit('resetFinish');
     }
     socket.emit('login', { hello: socket.id + ', 恭喜链接成功！可选择餐厅：' + SelectDinner.diningRooms.join(',') });
-    if (SelectDinner.selected.length >= 0 && SelectDinner.selected.length < 2) {
+    if (SelectDinner.selected.length >= 0 && SelectDinner.selected.length < 3) {
       socket.emit('selected', {selected: '已选择餐厅：' + SelectDinner.selected.join(',')});
-    } else if (SelectDinner.selected.length === 2 && !SelectDinner.lastSelect) {
+    } else if (SelectDinner.selected.length === 3 && !SelectDinner.lastSelect) {
       socket.emit('selected', {selected: '已选择餐厅：' + SelectDinner.selected.join(',')});
       socket.emit('lastSelected', {selected: '最终选择餐厅：' + SelectDinner.lastSelect});
       resetDinnerTime();
-    } else if (SelectDinner.selected.length === 2 && SelectDinner.lastSelect) {
+    } else if (SelectDinner.selected.length === 3 && SelectDinner.lastSelect) {
       socket.emit('selected', {selected: '已选择餐厅：' + SelectDinner.selected.join(',')});
       socket.emit('lastSelected', {selected: '最终选择餐厅：' + SelectDinner.lastSelect});
       socket.emit('disabled');
+      resetDinnerTime();
     } else {
       socket.emit('selected', {selected: '已选择餐厅：' + SelectDinner.selected.join(',')});
       socket.emit('lastSelected', {selected: '最终选择餐厅：' + SelectDinner.lastSelect});
       io.sockets.emit('disabled');
+      resetDinnerTime();
     }
     socket.on('select', function (data) {
-      if (SelectDinner.selected.length < 2) {
-        SelectDinner.selectRooms(SelectDinner.diningRooms, 7);
+      if (SelectDinner.selected.length < 3) {
+        SelectDinner.selectRooms(SelectDinner.diningRooms, 6);
         io.sockets.emit('selected', {selected: '已选择餐厅：' + SelectDinner.selected.join(',')});
         socket.emit('disabled');
-      } else if (SelectDinner.selected.length === 2)  {
-        SelectDinner.selectRooms(SelectDinner.selected, 2);
+      } else if (SelectDinner.selected.length === 3)  {
+        SelectDinner.selectRooms(SelectDinner.selected, 3);
         io.sockets.emit('lastSelected', {selected: '最终选择餐厅：' + SelectDinner.lastSelect});
         io.sockets.emit('disabled');
       } else {
@@ -75,10 +77,10 @@ io.on('disconnection', function (socket) {
 const SelectDinner = {
   lastSelect: '',
   selected: [],
-  diningRooms: ['重庆小面', '蒸菜', '面必居', '楼下炒菜', '罗森', '饭小餐', '陕老顺'],
+  diningRooms: ['鸡汤面', '沙县小吃', '面必居', '川香居', '罗森', '饭小餐'],
   selectRooms: (arr, num) => {
     const roomIndex = Math.floor(Math.random() * num);
-    if (num === 7) {
+    if (num === 6) {
       SelectDinner.selected.push(arr[roomIndex]);
     } else {
       SelectDinner.lastSelect = arr[roomIndex];
@@ -90,12 +92,17 @@ const SelectDinner = {
     SelectDinner.lastSelect = '';
   }
 };
+let setTimer = null;
 function resetDinnerTime () {
-  let nowDate = new Date();
-  const times1 = nowDate.getTime();
-  nowDate.setHours(24, 0, 0);
-  const times2 = nowDate.getTime();
-  setTimeout(() => {
-    SelectDinner.init();
-  }, times2 - times1)
+  if (!setTimer) {
+    let nowDate = new Date();
+    const times1 = nowDate.getTime();
+    nowDate.setHours(24, 0, 0);
+    const times2 = nowDate.getTime();
+    setTimer = setTimeout(() => {
+      SelectDinner.init();
+      setTimer = null;
+    }, times2 - times1);
+  }
+
 }
